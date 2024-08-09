58608deeb9d902ac14742a483a5366f86d4acd96
ansleybrown1337 <ansleybrown1337@gmail.com>
2024-08-09 15:34:04 -0600

    added documentation and started water balance

---
+++ code/lysimeter_analysis/water_balance.py
@@ -0,0 +1,7 @@
+# lysimeter_analysis/non_standard_events.py
+'''
+Script that converts load cell data (mV/V) to mass (kg) for each time step, and
+calculates the actual transpiration rate (mm/t) for each time step.
+
+
+'''
