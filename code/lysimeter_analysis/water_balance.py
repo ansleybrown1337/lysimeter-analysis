2ecc5caee5b1cbc46d04752a5171ec311855d7e0
ansleybrown1337 <ansleybrown1337@gmail.com>
2024-08-09 15:40:17 -0600

    Update water_balance.py
    This code works!!!

--- code/lysimeter_analysis/water_balance.py
+++ code/lysimeter_analysis/water_balance.py
@@ -3,5 +3,15 @@
 Script that converts load cell data (mV/V) to mass (kg) for each time step, and
 calculates the actual transpiration rate (mm/t) for each time step.
 
-
+The logical flow will be as follows:
+1. Load the data after being processed by the DatFileMerger and 
+NonStandardEvents classes.
+2. Convert the load cell data to mass using the calibration equation for each
+for each lysimeter type (i.e., SL or LL).
+3. Calculate the actual transpiration rate (ETa) for each time step using the mass data
+and the lysimeter area.
+4. Identify ETa rates that are immediately prior to and after each NSE, then
+linearly interpolate the ETa rate for the NSE and use that in place of the load
+cell -derived ETa rate.
+5. Use the ETa rates to calculate the water balance for the selected lysimeter.
 '''
